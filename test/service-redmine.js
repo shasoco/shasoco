3d807d2be321c91fe7eca40ac7fbd67bfe8428fb
Jean-Christophe Hoelt <hoelt@fovea.cc>
2015-12-11 23:20:43 +0100

    Add redmine service

---
+++ test/service-redmine.js
@@ -0,0 +1,45 @@
+var assert = require('assert');
+var sinon = require('sinon');
+var redmine = require('../lib/service/redmine.js');
+var utils = require('../lib/utils.js');
+
+describe('Redmine', function() {
+
+    var compose;
+    before(function() {
+        compose = sinon.stub(redmine.mods.domains, "compose");
+    });
+    after(function() {
+        compose.restore();
+    });
+
+    describe('#up()', function() {
+        it('should startup postgresql and redmine', function() {
+            redmine.up({});
+            assert(utils.hasWords(['up', 'redminepostgresql'],
+                                  compose.lastCall.args[1]));
+            assert(utils.hasWords(['up', 'redmine'],
+                                  compose.lastCall.args[1]));
+        });
+    });
+
+    describe('#backup()', function() {
+        it('should pg_dump from redminebackup', function() {
+            redmine.backup({
+                backupPath: "/test"
+            });
+            assert(utils.hasWords([ 'run', 'redminebackup', 'pg_dump' ],
+                                  compose.lastCall.args[1]));
+        });
+    });
+
+    describe('#restore()', function() {
+        it('should psql from redminebackup into postgresql', function() {
+            redmine.restore({
+                backupPath: "/test"
+            });
+            assert(utils.hasWords([ 'run', 'redminebackup', 'psql' ],
+                                  compose.lastCall.args[1]));
+        });
+    });
+});
--- test/services.js
+++ test/services.js
@@ -1,1 +1,8 @@
-require('../lib/services.js');
+var assert = require('assert');
+var services = require('../lib/services.js');
+
+describe('Services', function() {
+    it('should expose all services', function() {
+        assert.equal(6, services.length);
+    });
+});
