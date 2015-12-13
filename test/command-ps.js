fa50faa8629e06eea65b6ee37a89f4e62956aed3
Jean-Christophe Hoelt <hoelt@fovea.cc>
2015-12-13 15:31:39 +0100

    Cleanup the metaproxy feature

--- test/command-ps.js
+++ test/command-ps.js
@@ -16,7 +16,7 @@ describe('Ps', function() {
         it('should log running deploys', function() {
             ps.mods.console = { log: sinon.spy() };
             var list = function() {
-                return [{ id: 'a', port: 80, version: 'b', status: 'UP' }];
+                return [{ id: 'a', ports: "22,80,443", version: 'b', status: 'UP' }];
             };
             ps.mods.deploys = {
                 list: list
@@ -25,8 +25,8 @@ describe('Ps', function() {
             assert.equal(2, ps.mods.console.log.getCalls().length);
             var l0 = ps.mods.console.log.args[0][0];
             var l1 = ps.mods.console.log.args[1][0];
-            assert(l0.match(/ *ID *PORT *VERSION *STATUS/));
-            assert(l1.match(/ *a *80 *b *UP/));
+            assert(l0.match(/ *ID *PORTS *VERSION *STATUS/));
+            assert(l1.match(/ *a *22,80,443 *b *UP/));
         });
     });
 });
